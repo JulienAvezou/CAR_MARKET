9364ee36c50fa4fabd2c45a531e22eeca6beff06
Julien Avezou <julien.avezou@gmail.com>
2021-01-06 23:37:43 +0100

    :fire: car model & controller & routes done

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   
   has_one_attached :image
+  has_many :cars
   
   validates :email, presence: true, uniqueness: true
   validates :first_name, presence: true
