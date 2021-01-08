7cd384b5e97e92a56aa8f452129cd39cfa6aa9d3
Julien Avezou <julien.avezou@gmail.com>
2021-01-08 12:41:46 +0100

    :fix: request update fixed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,9 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @current_user = current_user
     @cars = @user.cars
+    check = false
+    Invitation.all.each { |invitation| check = true if (invitation.user_id == @user.id || invitation.user_id == current_user.id)  && (invitation.friend_id == @user.id || invitation.friend_id == current_user.id) && invitation.confirmed == false }
+    @check = check
   end
 
 end
